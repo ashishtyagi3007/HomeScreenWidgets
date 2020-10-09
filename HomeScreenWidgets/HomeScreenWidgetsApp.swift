62eb7e317ca9d760b8d4d2e93f851e205c2fca67
Ashish Tyagi <ashisht@dewsolutions.in>
2020-10-09 23:15:43 +0530

    Initial Commit

---
+++ HomeScreenWidgets/HomeScreenWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeScreenWidgetsApp.swift
+//  HomeScreenWidgets
+//
+//  Created by Ashish Tyagi on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct HomeScreenWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
